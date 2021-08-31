ebf21a98a970ac892124e235102d656d18138dd4
Marcus <marcusvmelodev@gmail.com>
2021-08-30 21:20:19 -0300

    Created cargo package

---
+++ target/debug/deps/yac8e-9ae54eef83134ce2.d
@@ -0,0 +1,5 @@
+/home/marcusvm/Code/yac8e/yac8e/target/debug/deps/yac8e-9ae54eef83134ce2: src/main.rs
+
+/home/marcusvm/Code/yac8e/yac8e/target/debug/deps/yac8e-9ae54eef83134ce2.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/yac8e.d
@@ -0,0 +1,1 @@
+/home/marcusvm/Code/yac8e/yac8e/target/debug/yac8e: /home/marcusvm/Code/yac8e/yac8e/src/main.rs
